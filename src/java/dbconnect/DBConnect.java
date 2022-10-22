1ccb1fd76bb2560f278eb0b614224b555845ccbf
dinhducfuhl <ductdhe141624@fpt.edu.vn>
2022-10-22 09:49:01 +0700

    updated css

--- src/java/dbconnect/DBConnect.java
+++ src/java/dbconnect/DBConnect.java
@@ -6,11 +6,11 @@ import java.sql.SQLException;
 
  
 public class DBConnect {
-    private static final String DB_URL = "jdbc:sqlserver://LAPTOP-AFFRQ6U2\\SQLEXPRESS:1433;"
+    private static final String DB_URL = "jdbc:sqlserver://DESKTOP-A0E4T1K\\MAYAO:1433;"
             + "databaseName=vnprox;"
             + "encrypt=false";
     private static final String USER_NAME = "sa";
-    private static final String PASSWORD = "123456";
+    private static final String PASSWORD = "Trandinhduc137";
 
     public static Connection getConnection() {
         Connection conn = null;
